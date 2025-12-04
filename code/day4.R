ae80223b0c33ea4a65b0c5e73db964133f11665e
Valentin Lucet <valentin.lucet@gmail.com>
2025-12-04 00:29:20 -0500

    part 1

--- code/day4.R
+++ code/day4.R
@@ -1,8 +1,18 @@
 library(tidyverse)
 library(testthat)
 
-read_data <- function(input_file) {
+pad_matrix <- function(m, nrows, ncols) {
+  out <- matrix(NA, nrow = nrows, ncol = ncols)
+  out[2:(nrow(out)-1), 2:(ncol(out)-1)] <- m
+  out
+}
 
+read_data <- function(input_file) {
+  m <-read_lines(input_file) |>
+    str_replace_all(pattern = fixed("."), replacement = "0") |>
+    str_replace_all(pattern = fixed("@"), replacement = "1") |>
+    str_split("") |> sapply(as.numeric) |> t() 
+  pad_matrix(m, nrow(m)+2, ncol(m)+2)
 }
 
 # Part 1
@@ -10,7 +20,21 @@ read_data <- function(input_file) {
 test_dat <- read_data("inputs/day4_test.txt")
 dat <- read_data("inputs/day4.txt")
 
+get_ngb <- function(x, nc, nr) {
+  c(x-1, x+1, 
+    x-nc-1, x-nc, x-nc+1, 
+    x+nc+1, x+nc, x+nc-1
+  )
+}
+
 part1 <- function(d) {
+
+  non_NA <- which(d == 1)
+  ngbs <- lapply(non_NA, get_ngb, nc = ncol(d), nr = nrow(d))
+  vals <- lapply(ngbs, \(x, mat = d) mat[x])
+  sums <- lapply(vals, \(x) sum(x, na.rm = T))
+
+  return(sum(sums<4))
 
 }
 
